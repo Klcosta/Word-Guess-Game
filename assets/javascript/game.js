db9016fcc879f0e9c2cd21d957f84cbac2bc11ed
Costa <keith@Keiths-MacBook-Pro.local>
2019-02-25 18:44:18 -0500

    game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
 //World Capitals:
-var words = ["madrid", "london", "paris", "vientiane", "quito", "tashkent", "jakarta", "accra", "tahiti","lilongwe", "santiago", "lima", "algeirs", "dublin" , "manila", "bogota", "istambul", "dakar", "bern", "lisbon", "rome", "ottowa", "brasilia", "bangkok", "beijing", "tokyo", "islamabad", "rabat", "kigali",]
+var words = ["madrid", "london", "paris", "vientiane", "quito", "tashkent", "jakarta", "accra", "tahiti","lilongwe", "santiago", "lima", "algeirs", "dublin" , "manila", "bogota", "istambul", "dakar", "bern", "lisbon", "rome", "ottowa", "brasilia", "bangkok", "beijing", "tokyo", "islamabad", "rabat", "kigali", "amsterdam", "prague","berlin","moscow","doha","ulaanbaatar","abuja","managua","tehran","wellington","canberra","brussels","belgrade", "taipei", "cairo","beirut","nassau","seoul", "kathmandu", "male", ]
 
 //Scoring Variables
 var wins = 0;
-var remguesses = 10;
+var remguesses = 8;
 var badguesses = [];
+var counter = 0;
 
 //answer = Array with "_,_,_,_,"
 //letter = guessing word in array form
@@ -42,7 +43,7 @@ function checkletter(x) {
     //Choosing a Word
     //Choosing the Word
     function start(){
-        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("wins").innerHTML = wins + "/" + counter;
         document.getElementById("remainingguesses").innerHTML = remguesses;
         var randomnumber = Math.floor(Math.random() * words.length);
         var guessingword = words[randomnumber];
@@ -88,12 +89,14 @@ function checkletter(x) {
     //You Loose
     if (remguesses === 0) {
         console.log("you loose")
+        counter++;
         badguesses = [];
-        remguesses = 10;
+        remguesses = 8;
         document.getElementById("guessedletters").innerHTML = badguesses;
         document.getElementById("remainingguesses").innerHTML = remguesses;
         console.log(badguesses)
         console.log(remguesses)
+        console.log(counter)
         start()
     } 
     
@@ -106,15 +109,44 @@ function checkletter(x) {
     if(answerjoined === lettersjoined){
         console.log("youwin");
         wins ++;
+        counter++;
         badguesses = [];
-        remguesses = 10;
+        remguesses = 8;
         document.getElementById("guessedletters").innerHTML = badguesses;
         document.getElementById("remainingguesses").innerHTML = remguesses;
-        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("wins").innerHTML = wins + "/" + counter;
+        console.log(counter)
         start();
     }
 
     //You Keep Going
+
+    //When you've played 20 words
+
+    if(counter === 20){
+        console.log("Game Over")
+        if (wins > 19){
+            document.getElementById("cityimg").src = "assets/images/scoreA.jpg";
+        } 
+        if (wins === 17 || wins === 18) {
+            document.getElementById("cityimg").src = "assets/images/scoreB.jpg";
+        }
+        if (wins === 15 || wins === 16) {
+            document.getElementById("cityimg").src = "assets/images/scoreC.jpg";
+        }
+        if (wins === 13 || wins === 14) {
+            document.getElementById("cityimg").src = "assets/images/scoreD.jpg";
+        }
+        if (wins < 12) {
+            document.getElementById("cityimg").src = "assets/images/scoreF.jpg";
+        }
+        
+        counter = -1
+        wins = 0
+        remguesses = 1
+    } 
+
+
 
 }
 
